145eae4868e4794af3fd2bdf2cbab56ee4062027
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2025-07-09 16:31:11 -0300

    update

--- src/Connect.php
+++ src/Connect.php
@@ -67,7 +67,7 @@ class Connect
       return true;
     } catch (PDOException $e){
       return Message::error(
-        LogType::database, $e->getMessage()
+        LogType::database, $e->getMessage() . " - " . $this->dnsProps->text
       );
     }
   }
