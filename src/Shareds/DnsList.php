14e0aeae9ada8b81f43ec5b7ced15e47ee720440
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2025-10-03 15:21:27 -0300

    Update componentes

--- src/Shareds/DnsList.php
+++ src/Shareds/DnsList.php
@@ -12,7 +12,10 @@ class DnsList
   public static function load(
   ): DnsList {
     if(file_exists(rootdir . DIRECTORY_SEPARATOR . ".env") === false){
-      DnsList::$dnsList = DataList::create([]);
+      DnsList::$dnsList = (
+        DataList::create([])
+      );
+      
       return new static;
     }
 
